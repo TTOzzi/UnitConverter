67538b8c010f151aca3fb338dd61c6ab9c977bbf
TTOzzi <devttozzi@gmail.com>
2019-12-21 12:19:03 +0900

    숫자와 단위 분리, 단위 변환 구현
    문자열에서 숫자를 먼저 분리한 뒤 숫자부분을 지워서 단위를 얻음
    숫자를 분리한 방법으로 문자를 똑같이 분리하려하니 소숫점이 함께 분리되어 이 방법을 사용함

--- UnitConverter/UnitConverter/main.swift
+++ UnitConverter/UnitConverter/main.swift
@@ -5,8 +5,34 @@
 //  Created by TTOzzi on 2019/12/20.
 //  Copyright © 2019 TTOzzi. All rights reserved.
 //
-
 import Foundation
 
-print("Hello, World!")
+let cm = "120cm"
+var m = "1.86m"
 
+func separate(value: String) -> [String] {
+    var tmp = value
+    var ret = [String]()
+    ret.append(tmp.components(separatedBy: CharacterSet.letters).joined())
+    for _ in 1...ret[0].count {
+        tmp.removeFirst()
+    }
+    ret.append(tmp)
+    return ret
+}
+
+func convert(value: [String]) -> String {
+    var number = Double(value[0])!
+    var unit = value[1]
+    switch unit {
+    case "cm":
+        number = number / 100
+        unit = "m"
+    case "m":
+        number = number * 100
+        unit = "cm"
+    default:
+        break
+    }
+    return String(number) + unit
+}
